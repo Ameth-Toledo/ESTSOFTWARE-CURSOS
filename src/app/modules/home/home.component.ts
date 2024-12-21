ec9acff4156477f0b6f87bbdc34ff4f96af30d27
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2024-12-20 20:20:12 -0600

    responsividad

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,11 +3,13 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { TerminalComponent } from "../../components/terminal/terminal.component";
 import { CardCursosComponent } from "../../components/card-cursos/card-cursos.component";
 import { Router } from '@angular/router';
+import { BannerComponent } from "../../components/banner/banner.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, TerminalComponent, CardCursosComponent],
+  imports: [HeaderComponent, TerminalComponent, CardCursosComponent, BannerComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
