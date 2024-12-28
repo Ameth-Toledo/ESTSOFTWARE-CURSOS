4bef4642914c1846539fa94d95a5f7955344ffbf
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2024-12-27 22:15:27 -0600

    home cambios

--- src/app/modules/Html/actividad01-html/actividad01-html.component.ts
+++ src/app/modules/Html/actividad01-html/actividad01-html.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../components/header/header.component";
 import { FooterComponent } from "../../../components/footer/footer.component";
 import { Router } from '@angular/router';
+import { CardComandosComponent } from "../../../components/card-comandos/card-comandos.component";
 
 @Component({
   selector: 'app-actividad01-html',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CardComandosComponent],
   templateUrl: './actividad01-html.component.html',
   styleUrl: './actividad01-html.component.scss'
 })
