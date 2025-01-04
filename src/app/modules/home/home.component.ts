d66c232cbec11646f39e9a64e542d5d7a4406f18
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2025-01-03 21:35:14 -0600

    paypal donation

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,21 +1,68 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { TerminalComponent } from "../../components/terminal/terminal.component";
 import { CardCursosComponent } from "../../components/card-cursos/card-cursos.component";
 import { Router } from '@angular/router';
 import { BannerComponent } from "../../components/banner/banner.component";
 import { FooterComponent } from "../../components/footer/footer.component";
-
+import { PaypalService } from '../../services/paypal/paypal.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, TerminalComponent, CardCursosComponent, BannerComponent, FooterComponent],
+  imports: [FormsModule, HeaderComponent, TerminalComponent, CardCursosComponent, BannerComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  donationAmount : number = 0;
 
-  constructor (private router : Router) {}
+  constructor (private router : Router, private paypalService: PaypalService) {}
+
+  ngAfterViewInit(): void {
+    this.initializePaypalButton();
+  }
+  
+  private initializePaypalButton() {
+    this.paypalService.loadPaypalScript().then(() => {
+      // @ts-ignore: PayPal object is loaded dynamically
+      paypal.Buttons({
+        style: {
+          layout: 'vertical',
+          color: 'blue',
+          shape: 'rect',
+          label: 'donate',
+        },
+        createOrder: (data: any, actions: any) => {
+          if (this.donationAmount <= 0) {
+            alert('Por favor, ingresa un monto válido para donar.');
+            throw new Error('Monto de donación inválido');
+          }
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: this.donationAmount.toFixed(2), 
+                  currency_code: 'MXN',
+                },
+              },
+            ],
+          });
+        },
+        onApprove: (data: any, actions: any) => {
+          return actions.order.capture().then((details: any) => {
+            alert(`Gracias por tu donación, ${details.payer.name.given_name}!`);
+          });
+        },
+        onError: (err: any) => {
+          console.error('Error en la transacción de PayPal:', err);
+          alert('Hubo un error procesando tu donación. Por favor, intenta nuevamente.');
+        },
+      }).render('#paypal-button-container');
+    }).catch(err => {
+      console.error('Error al cargar el script de PayPal:', err);
+    });
+  }
 
   sendCourseLinux(event : Event) {
     event.preventDefault();
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    PAYPAL_CLIENT_ID: 'AQV_1QIahnWs0Ry5TZ-BO2Cxz_ZRmTgX4NlI2y7gvenjiXv51lpp2zB_kj7LuQVnffH8_lhqIQzRDQBL'
+};
