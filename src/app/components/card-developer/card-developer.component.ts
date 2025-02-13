3ea74445717d9bf4967ba4b4e7cfab36e1ff7251
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2025-02-12 22:58:56 -0600

    dev new

---
+++ src/app/components/card-developer/card-developer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-developer',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-developer.component.html',
+  styleUrl: './card-developer.component.scss'
+})
+export class CardDeveloperComponent {
+
+}
